009cae9ba386edc523bfa48ab0d38f39394c8106
Yordanis Pelaez <yordanispelaez@gmail.com>
2019-08-01 16:13:11 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,15 @@
 
+a = input("Enter a number: ")
+b = input("Enter other number: ")
+c = input("Enter calculation (s) sum (r) rest (m) mult (d) div:")
+
+if c = ("s"):
+    print(a + b)
+elif c = ("r"):
+    print(a - b)
+elif c = ("m"):
+    print(a * b)
+elif c = ("d"):
+    print(a / b)
+else: print("Try again")
+
